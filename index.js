0d0149f7a0c96488c673eb1b1ad2147bca24d2f5
Sarvesh <sarveshregmi1@gmail.com>
2023-11-16 23:07:07 -0600

    Assignment completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const defaultName=[
+const defaultName=[         //default names that will be displayed first
     {
         Name:"Alice",
         Occupation:"Writer",
@@ -10,50 +10,68 @@ const defaultName=[
         Price: 50,
     }
 ]
-let freelancers = [
-    { Name: "Dr. Slice", Price: 25, Occupation: "gardener" },
-    { Name: "Dr. Pressure", Price: 51, Occupation: "programmer" },
-    { Name: "Prof. Possibility", Price: 43, Occupation: "teacher" },
-    { Name: "Prof. Prism", Price: 81, Occupation: "teacher" },
-    { Name: "Dr. Impulse", Price: 43, Occupation: "teacher" },
-    { Name: "Prof. Spark", Price: 76, Occupation: "programmer" },
-    { Name: "Dr. Wire", Price: 47, Occupation: "teacher" },
-    { Name: "Prof. Goose", Price: 72, Occupation: "driver" },
-  ];
-  let counter=0;
-  let sum=0;
-debugger
 
-const addIntervalId=setInterval(addName,3000);
+let randomName=["Sam", "Pam", "Ammy", "Jimmy", "Jeff", "Mark", "Satya"];        //array of random names
+let randomOccupation=["Driver", "Carpenter", "Waiter", "Cook", "Bartender", "Rapper", "Athlete"];       //array of random profession
 
-render();
+const addIntervalId=setInterval(addName,3000);      //calls a function every 3 sec to add random freelancer to the table    
+
+render();       //gets called in once to render the webpage
+
+function averagePrice(){        //function to calculate average price
+    let total=0;
+    for(let i=0;i<defaultName.length;i++){
+        total+=defaultName[i]["Price"];         //gets the value of the price key for each object that are inside the array defaultName
+    }
+    return total/defaultName.length;
+}
+function displayAverage(){
+    const a=document.querySelector("#average");
+    a.textContent="The average price is $"+averagePrice().toFixed(2);       //passes the average value to the HTML content
+}
 
 function render(){
-    const mylist=document.querySelector("#list");
-    for(let i=0;i<defaultName.length;i++){
-        const node=document.createElement("li");
-    node.textContent=`${defaultName[i].Name}    ${defaultName[i].Occupation}    $${defaultName[i].Price}`; 
-    mylist.append(node);
-    sum+=defaultName[i].Price;
-    counter++;
-}
-const myavg=document.querySelector("#average");
-myavg.innerHTML=`The average starting price is $${(sum/counter).toFixed(2)}`;
+    const mylist=document.querySelector("#tbody");
+    for(let i=0;i<defaultName.length;i++){              //loops through the defaultName array to grab the value of each key
+        const node=document.createElement("tr");
+        const nodeName=document.createElement("td");
+        const nodeOccupation=document.createElement("td");
+        const nodePrice=document.createElement("td");
+        nodeName.textContent=defaultName[i]["Name"];        //assign the value to the newly created table data
+        nodeOccupation.textContent=defaultName[i]["Occupation"];
+        nodePrice.textContent="$ "+defaultName[i]["Price"];
+        node.append(nodeName);          //add the table data to the table row
+        node.append(nodeOccupation);
+        node.append(nodePrice);
+        mylist.append(node);            //finally add the table row to the table
+        displayAverage();               //update the average display each time a new row is added
+    }   
 }
 
 function addName(){
-    if(counter>9){
+    let d=new Date();       //date is used to create random price
+    let randomNumber=Math.floor((Math.random()*(randomName.length-1)));     //create a random number to select a random name
+    let randomNumber1=Math.floor((Math.random()*(randomName.length-1)));    //create a random number to select a random occupation
+    let randomObj={Name: randomName[randomNumber], Occupation: randomOccupation[randomNumber1], Price: (10+d.getSeconds()+(randomNumber*randomNumber1))};   //assign the random name, occupation and price to an object
+    defaultName.push(randomObj);        //push that new object to the defaultName array so that accurate average price can be calculated
+    const mylist=document.querySelector("#tbody");      //repeat the same step as in the render() function to add data to the table
+    const node=document.createElement("tr");
+    const nodeName=document.createElement("td");
+    const nodeOccupation=document.createElement("td");
+    const nodePrice=document.createElement("td");
+    nodeName.textContent=randomObj["Name"];
+    nodeOccupation.textContent=randomObj["Occupation"];
+    nodePrice.textContent="$ "+randomObj["Price"];
+    node.append(nodeName);
+    node.append(nodeOccupation);
+    node.append(nodePrice);
+    mylist.append(node);
+    displayAverage();
+    let x=randomName.splice(randomNumber,1);        //remove the random name and occupation that were last used so that they are not repeated again
+    let y=randomOccupation.splice(randomNumber1,1);
+    if(randomName.length===0){          //stop recalling the addName function when all the names inside the randomName array has been used
+        console.log("List Complete");
         clearInterval(addIntervalId);
     }
-    const n=Math.floor(Math.random()*8);
-    const a= freelancers[n];
-    const mylist=document.querySelector("#list");
-    const node=document.createElement("li");
-    node.textContent=`${a.Name}   ${a.Occupation}    $${a.Price}`; 
-    mylist.append(node);
-    sum+=a.Price;
-    delete freelancers[n];
-    counter++;
-    const myavg=document.querySelector("#average");
-    myavg.innerHTML=`The average starting price is $${(sum/counter).toFixed(2)}`;
 }
+
